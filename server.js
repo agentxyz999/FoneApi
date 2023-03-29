2e587c50767e695edb0126a54fd41d5ef0f42b6e
Rolie Conde <onefivezero999@gmail.com>
2023-03-29 23:59:55 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const server = app.listen(8004, () => console.log("listening on port 8004"));
+const io = require("socket.io")(server);
+const users = {};
+
+app.use(express.static(__dirname + "/static"));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+//when user is connected
+io.on("connection", function (socket) {
+  socket.on("new_user", (user_name) => {
+    io.emit("connected_user", { user_name: user_name, socket_id: socket.id });
+  });
+
+  //when user sends a message
+  socket.on("sending_msg", (data) => {
+    if (data.recipient === "Everyone") {
+      io.emit("send_msg_all", { data }); //broadcast to all connected users
+      for (x in users) {
+        console.log(users[x]);
+      }
+    }
+  });
+});
+//render the index page including the users
+app.get("/", function (req, res) {
+  res.render("index", { users });
+});
---
+++ static/js/index.js
@@ -0,0 +1,44 @@
+$(window).on("load", function () {
+  let user_name = prompt("What is your name? ");
+  $(document).ready(function (e) {
+    const socket = io();
+    //new user connected
+    socket.emit("new_user", user_name);
+    console.log("new user connected", user_name);
+    socket.on("connected_user", (data) => {
+      let p = document.createElement("p");
+      p.innerText = data.user_name;
+      $("#user-list").append(p);
+      // console.log(data.socket_id);
+    });
+
+    //send button clicked / form submitted
+    $("#msg-form").on("submit", function (e) {
+      e.preventDefault();
+      let recipient = document.getElementById("recipient");
+      let msg = document.getElementById("msg");
+      msg = msg.value;
+      let recipient_name = recipient.value;
+      socket.emit("sending_msg", {
+        user_name: user_name,
+        msg: msg,
+        recipient: recipient_name,
+      });
+      $(this).trigger("reset"); //reset chatbox after sending msg
+      return false;
+    });
+
+    socket.on("send_msg_all", function (data) {
+      let h2 = document.createElement("h2");
+      let p = document.createElement("p");
+      //console.log(data);
+      for (x in data) {
+        h2.innerText = `${data[x].user_name} to Everyone: \n`;
+        if (data.data.msg) {
+          p.innerText = `${data[x].msg}`;
+          $("#user-msg").append(h2).append(p);
+        }
+      }
+    });
+  });
+});
